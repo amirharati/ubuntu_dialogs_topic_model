6f4040b3662b509b548a594dea632d7717fc23a0
amir <amir.harati@gmail.com>
2018-03-18 15:59:49 -0700

    update train_doc2vec_model

--- train_doc2vec_model.py
+++ train_doc2vec_model.py
@@ -12,15 +12,13 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s'
 
 cores = multiprocessing.cpu_count() - 1
 
-#tagged_docs = models.doc2vec.TaggedLineDocument("data/dialogs_4.txt")
-
 fo = open("tmp/dialogs4-tagged-corpus.pickle", 'rb')
 tagged_docs = pickle.load(fo)
 fo.close()
-#tagged_docs = UC.UbuntuCorpus("data/dialogs_4.txt", "tmp/dialogs4.dict", tagged_document=True)
-model = models.doc2vec.Doc2Vec(dm=0, alpha=0.025, dm_mean=1, size=10, window=10, negative=5, hs=0, min_count=2, workers=cores)  # use fixed learning rate
+
+model = models.doc2vec.Doc2Vec(dm=0, alpha=0.025, dm_mean=1, size=50, window=10, negative=5, hs=0, min_count=2, workers=cores)  # use fixed learning rate
 model.build_vocab(tagged_docs)
-for epoch in range(10):
+for epoch in range(30):
     model.train(tagged_docs, total_examples=model.corpus_count, epochs=1)
     model.alpha -= 0.002  # decrease the learning rate
     model.min_alpha = model.alpha  # fix the learning rate, no decay
